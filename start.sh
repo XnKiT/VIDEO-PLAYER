f4ecf9f7737f042602d3bae266a93e03894729b4
⚝ANKIT KUMAR⚝ <97168788+LEGEND-ANKIT@users.noreply.github.com>
2022-01-14 01:28:43 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-echo "Cloning Repo..."
-git clone -b main https://github.com/LEGEND-ANKIT/VIDEO-PLAYER /VIDEO-PLAYER
-cd /VIDEO-PLAYER
-
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-
-echo "Starting Bot..."
-python3 vidstream.py
