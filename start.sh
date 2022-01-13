044d08f33eb16852077fbcce3f02faf56a06236f
⚝ANKIT KUMAR⚝ <97168788+LEGEND-ANKIT@users.noreply.github.com>
2022-01-14 00:24:05 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-echo "Cloning Repo..."
-git clone -b main https://github.com/LEGEND-ANKIT/TELEGRAM-VC-STREAMER /Ankit
-cd /Ankit
-
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-
-echo "Starting Bot..."
-python3 ankit.py
