99c16bde22019bd837e26da90c870ed6bd2735aa
⚝ANKIT KUMAR⚝ <97168788+LEGEND-ANKIT@users.noreply.github.com>
2022-01-14 00:49:28 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-echo "Cloning Repo..."
-git clone -b main https://github.com/LEGEND-ANKIT/TELEGRAM-VC-STREAMER /VideoBot
-cd /VideoBot
-
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-
-echo "Starting Bot..."
-python3 vidstream.py
